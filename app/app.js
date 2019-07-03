19bce3e1de7198d34a6376f7f8591f9da2a63f34
CressZZ <dktsjssnrnsi@gmail.com>
2019-07-03 22:34:00 +0900

    test

--- app/app.js
+++ app/app.js
@@ -1,8 +1,14 @@
 let express = require('express');
 let app = express();
+const path = require('path');
+
+app.use(express.static(__dirname));
+// app.use(express.static('styles'));
+
 
 app.get('/', function(req, res) {
-    res.render('index.html');
+    res.sendFile(path.join(__dirname+'/index.html'));
+    console.log;
 });
 
 app.listen(3000, function() {
