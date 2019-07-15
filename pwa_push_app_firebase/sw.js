93981ffc6c23b00bf80c4daeab2feaed539434f5
Youngjin Park <yjpark0228@ncsoft.com>
2019-07-15 11:05:22 +0900

    test

--- pwa_push_app_firebase/sw.js
+++ pwa_push_app_firebase/sw.js
@@ -23,8 +23,8 @@
 let CACHE_NAME = 'test-cache-v1';
 let CACHED_URLS = [
     '/index-offline.html',
+    'https://cress00-pwa.s3.ap-northeast-2.amazonaws.com/pwa/script/main.js',
     'https://cress00-pwa.s3.ap-northeast-2.amazonaws.com/pwa/styles/index.css',
-    'https://cress00-pwa.s3.ap-northeast-2.amazonaws.com/pwa/script/main.js'
 ]
 self.addEventListener('push', function (event) {
     console.log('[Service Worker] Push Received.');
