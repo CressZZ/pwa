9fe66467e87296dab03dea224c5bfa71cd2d8081
Youngjin Park <yjpark0228@ncsoft.com>
2019-07-15 13:58:14 +0900

    test

--- pwa_push_app_firebase/sw.js
+++ pwa_push_app_firebase/sw.js
@@ -64,8 +64,7 @@ self.addEventListener('install', event => {
         caches.open(CACHE_NAME)
         .then((cache)=> {
             return cache.add('./index-offline.html')
-                .then(()=>cache.add('https://cress00-pwa.s3.ap-northeast-2.amazonaws.com/pwa/script/main.js'))
-                .then(()=>cache.add('https://cress00-pwa.s3.ap-northeast-2.amazonaws.com/pwa/styles/index.css'))
+                .then(()=>cache.add('https://code.jquery.com/jquery-3.4.1.js'))
         })
         // .then(cache => {
         //     return cache.addAll(CACHED_URLS)
